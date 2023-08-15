8b084eb85aa3262da93fdf424660cf421944dab0
Ada <ada@chronovore.dev>
2023-08-15 16:31:17 +0100

    rewrite part of rivet_array to make slice zero-copy

--- include/rivet/rivet_array.hpp
+++ include/rivet/rivet_array.hpp
@@ -4,64 +4,61 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <functional>
 #include <iterator>
 #include <memory>
+#include <new>
 #include <sstream>
 #include <string>
-#include <vector>
+#include <utility>
 
 #include <rivet/rivet_keywords.hpp>
 
 namespace rivet {
 	template<typename T, const int Alignment>
 	struct rivet_array {
-		std::shared_ptr<T[]> ptr = nullptr;
+		std::shared_ptr<uint8_t[]> ptr = nullptr;
 		std::size_t length = 0;
+		std::size_t offset = 0;
 
-		struct Iterator {
+		struct iterator {
 			using iterator_category [[maybe_unused]] = std::forward_iterator_tag;
 			using difference_type [[maybe_unused]] = std::ptrdiff_t;
 			using value_type = T;
 			using pointer = value_type *;
 			using reference = value_type &;
 
-			explicit Iterator(pointer ptr) : array_ptr(ptr) { }
+			explicit iterator(pointer ptr) : array_ptr(ptr) { }
 
 			reference operator*() const { return *array_ptr; }
 
 			pointer operator->() { return array_ptr; }
 
-			Iterator &operator++() {
+			iterator &operator++() {
 				array_ptr++;
 				return *this;
 			}
 
-			Iterator operator++(int) { // NOLINT(cert-dcl21-cpp)
-				Iterator tmp = *this;
+			iterator operator++(int) { // NOLINT(cert-dcl21-cpp)
+				iterator tmp = *this;
 				++(*this);
 				return tmp;
 			}
 
-			friend bool operator==(const Iterator &a, const Iterator &b) { return a.array_ptr == b.array_ptr; };
+			friend bool operator==(const iterator &a, const iterator &b) { return a.array_ptr == b.array_ptr; };
 
-			friend bool operator!=(const Iterator &a, const Iterator &b) { return a.array_ptr != b.array_ptr; };
+			friend bool operator!=(const iterator &a, const iterator &b) { return a.array_ptr != b.array_ptr; };
 
-		private:
 			pointer array_ptr;
 		};
 
-	private:
-		std::size_t offset = 0;
-
 		void alloc(size_t size) {
-			ptr = std::shared_ptr<T[]>(new (std::align_val_t(Alignment)) T[size]);
+			ptr = std::shared_ptr<uint8_t[]>(new (std::align_val_t(Alignment)) T[size * sizeof(T)]);
 		}
 
-	public:
 		rivet_array() {
 			length = 0;
 			offset = 0;
@@ -77,15 +74,17 @@ namespace rivet {
 		}
 
 		[[maybe_unused]] rivet_array(T *ptr, std::size_t size, const T &default_value) : rivet_array(ptr, size) {
+			auto buffer = data();
 			for (auto i = 0; i < length; ++i) {
-				ptr[i] = default_value;
+				buffer[i] = default_value;
 			}
 		}
 
-	public:
-		RIVET_INLINE T *data() const { return reinterpret_cast<T *>(reinterpret_cast<intptr_t>(ptr.get()) + offset); }
+		[[maybe_unused]] rivet_array(std::shared_ptr<uint8_t[]> ptr, std::size_t length, std::size_t offset) : ptr(std::move(ptr)), length(length), offset(offset) { }
 
-		[[nodiscard]] RIVET_INLINE bool is_aligned() const { return reinterpret_cast<intptr_t>(ptr.get() + offset) % Alignment; }
+		RIVET_INLINE T *data() const { return reinterpret_cast<T *>(ptr.get() + offset * sizeof(T)); }
+
+		[[maybe_unused]] [[nodiscard]] RIVET_INLINE bool is_aligned() const { return reinterpret_cast<intptr_t>(ptr.get() + offset) % Alignment; }
 
 		[[nodiscard]] RIVET_INLINE std::size_t size() const { return length; }
 
@@ -95,100 +94,60 @@ namespace rivet {
 
 		[[maybe_unused]] RIVET_INLINE T &operator[](std::size_t index) const { return get(index); }
 
-		Iterator begin() const { return Iterator(data()); }
+		iterator begin() const { return iterator(data()); }
 
-		Iterator end() const { return Iterator(data() + size()); }
-
-		[[maybe_unused]] void fill(const T &default_value) {
-			for (auto i = 0; i < length; ++i) {
-				ptr[i] = default_value;
-			}
-		}
+		iterator end() const { return iterator(data() + size()); }
 
 		[[maybe_unused]] T &get(std::size_t index) const {
 			assert(index < size());
+
 			return data()[index];
-		}
-
-		[[maybe_unused]] T &get(std::size_t& index) const {
-			assert(index < size());
-			return data()[index++];
-		}
-
-		[[maybe_unused]] void set(std::size_t index, T value) {
-			assert(index < size());
-			data()[index] = value;
-		}
-
-		[[maybe_unused]] void set(std::size_t& index, T value) {
-			assert(index < size());
-			data()[index++] = value;
 		}
 
 		template<typename U>
 		[[maybe_unused]] U get(std::size_t index) const {
 			assert(index < size());
 			assert(sizeof(T) * index + sizeof(U) < byte_size());
+
 			return reinterpret_cast<U *>(data() + index)[0];
 		}
 
-		template<typename U>
-		[[maybe_unused]] U get(std::size_t& index) const {
+		[[maybe_unused]] void set(std::size_t index, T value) {
 			assert(index < size());
-			assert(sizeof(T) * index + sizeof(U) < byte_size());
-			auto value = reinterpret_cast<U *>(data() + index)[0];
-			index += sizeof(U) / sizeof(T);
-			return value;
+
+			data()[index] = value;
 		}
 
 		template<typename U>
 		[[maybe_unused]] U set(std::size_t index, U value) {
 			assert(index < size());
 			assert(sizeof(T) * index + sizeof(U) < byte_size());
+
 			reinterpret_cast<U *>(data() + index)[0] = value;
 		}
 
-		template<typename U>
-		[[maybe_unused]] U set(std::size_t& index, U value) {
-			assert(index < size());
-			assert(sizeof(T) * index + sizeof(U) < byte_size());
-			reinterpret_cast<U *>(data() + index)[0] = value;
-			index += sizeof(U) / sizeof(T);
-		}
-
-		// todo: refactor so this can be zero copy.
-		[[maybe_unused]] [[nodiscard]] std::shared_ptr<rivet_array<T, Alignment>> slice(std::size_t index, std::size_t count) const {
+		[[maybe_unused]] [[nodiscard]] std::shared_ptr<rivet_array<T, Alignment>> slice(std::size_t &index, std::size_t count) const {
 			assert(index < size());
 			assert(index + count < size());
 
-			return std::make_shared<rivet_array<T, Alignment>>(data() + index, count);
+			std::make_shared<rivet_array<T, Alignment>>(ptr, count, offset + index);
 		}
 
 		template<typename U>
 		[[maybe_unused]] std::shared_ptr<rivet_array<U, Alignment>> slice(std::size_t index, std::size_t count) const {
 			assert(index < size());
 			assert(sizeof(T) * index + sizeof(U) * count < byte_size());
+			auto new_offset = (offset + index) * sizeof(T) / sizeof(U);
 
-			return std::make_shared<rivet_array<U, Alignment>>(reinterpret_cast<U *>(data() + index), count);
-		}
-
-		[[maybe_unused]] std::shared_ptr<rivet_array<T, Alignment>> slice(std::size_t& index, std::size_t count) const {
-			assert(index < size());
-			assert(index + count < size());
-
-			auto value = std::make_shared<rivet_array<T, Alignment>>(data() + index, count);
-			index += count;
-			return value;
+			return std::make_shared<rivet_array<T, Alignment>>(ptr, count, new_offset);
 		}
 
 		template<typename U>
-		[[maybe_unused]] std::shared_ptr<rivet_array<U, Alignment>> slice(std::size_t& index, std::size_t count) const {
-			assert(index < size());
-			assert(sizeof(T) * index + sizeof(U) * count < byte_size());
+		[[maybe_unused]] std::shared_ptr<rivet_array<U, Alignment>> cast() const {
+			auto new_size = length * sizeof(T) / sizeof(U);
+			auto new_offset = offset * sizeof(T) / sizeof(U);
 
-			auto value = std::make_shared<rivet_array<U, Alignment>>(reinterpret_cast<U *>(data() + index), count);
-			index += (sizeof(U) / sizeof(T)) * count;
-			return value;
+			return std::make_shared<rivet_array<U, Alignment>>(ptr, new_size, new_offset);
 		}
 
 		[[maybe_unused]] void copy_to(std::shared_ptr<rivet_array<T, Alignment>> &array, std::size_t index, std::size_t count) {
@@ -199,16 +158,6 @@ namespace rivet {
 			std::copy_n((data() + index), count, array->data());
 		}
 
-		template<typename U>
-		[[maybe_unused]] void copy_to(std::shared_ptr<rivet_array<U, Alignment>> &array, std::size_t index, std::size_t count) {
-			assert(array->size() > count);
-			assert(index < size());
-			assert(sizeof(T) * index + sizeof(U) * count < byte_size());
-
-			std::copy_n(reinterpret_cast<U *>(data() + index), count, array->data());
-		}
-
-
 		template<typename U = T>
 		[[maybe_unused]] typename std::enable_if<sizeof(U) <= 2 && std::is_same<U, T>::value && std::is_integral<U>::value, void>::type ensure_null_terminated() {
 			auto buffer = ptr;
@@ -217,7 +166,6 @@ namespace rivet {
 				alloc(length);
 				std::copy_n(buffer.get(), size() - 1, data());
 				data()[size() - 1] = static_cast<T>(0);
-				delete buffer;
 			}
 		}
 
