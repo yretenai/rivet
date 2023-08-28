af1c07d4eafbac5e92ec58c833924813bb97a00f
Ada <ada@chronovore.dev>
2023-08-28 17:44:00 +0100

    refactor localization handling

--- cli/main.cpp
+++ cli/main.cpp
@@ -13,7 +13,7 @@ using namespace rivet;
 using namespace rivet::structures;
 
 std::array<std::string, 32> localization_enum {
-		"none",
+		"",
 		"us",
 		"gb",
 		"dk",
@@ -79,12 +79,7 @@ int main(int argv, char **argc) {
 	}
 	dag_file.close();
 
-	for(auto locale_id = 0; locale_id < 32; locale_id++) {
-		std::string local_path;
-		if(locale_id > 0) {
-			local_path += "locale/" + localization_enum[locale_id] + "/";
-		}
-
+	for(auto locale_id = 1; locale_id < 32; locale_id++) {
 		auto locale = static_cast<rivet_locale>(locale_id);
 
 		for(auto category_id = 0; category_id < 4; category_id++) {
@@ -108,25 +103,30 @@ int main(int argv, char **argc) {
 						rivet::hash::normalize_asset_path(name);
 					}
 
+					if (name.ends_with("/localization_all.localization")) {
+						name = name.substr(0, name.find_last_of('/')) + "/localization_" + localization_enum[locale_id] + ".localization";
+					} else if (locale_id > 0) {
+						name = name.substr(0, name.find_last_of('/')) + "/" + localization_enum[locale_id] + "/" + name.substr(name.find_last_of('/') + 1);
+					}
+
 					auto asset_data = game->open_file(asset);
 					if (asset_data == nullptr || asset_data->empty()) {
 						std::cout << "Failed to open asset " << asset->name << std::endl;
 						continue;
 					}
 
-					auto asset_path = local_path + name;
 					if(subtype_id == 1 && asset->flags.is_texture) {
-						asset_path += ".stream";
+						name += ".stream";
 					}
 
-					std::cout << "Writing " << asset_path << std::endl;
+					std::cout << "Writing " << name << std::endl;
 
 					// rename .movie to .bik
 					if(asset_data->get<uint32_t>(0) == 0x6A32424B) {
-						asset_path = asset_path.substr(0, asset_path.find_last_of('.')) + ".bik";
+						name = name.substr(0, name.find_last_of('.')) + ".bik";
 					}
 
-					auto output_path = dump / asset_path;
+					auto output_path = dump / name;
 					std::filesystem::create_directories(output_path.parent_path());
 					std::ofstream asset_file(output_path, std::ios::binary | std::ios::out);
 
