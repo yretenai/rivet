b86b137e45fdc2264ae7e03fe04167f855bf8176
Ada <ada@chronovore.dev>
2023-09-11 14:36:26 +0100

    more morph research

--- docs/research/model_morph.c
+++ docs/research/model_morph.c
@@ -1,6 +1,6 @@
 struct morph_pair_entry {
-	/* 0x00 */ uint32_t left; // LF_[name]
-	/* 0x04 */ uint32_t right; // RT_[name]
+	/* 0x00 */ uint32_t left_id; // LF_[name]
+	/* 0x04 */ uint32_t right_id; // RT_[name]
 };
 
 struct morph_lookup_entry {
@@ -16,41 +16,43 @@ struct morph_header {
 	/* 0x0C */ uint32_t lookup_offset;
 };
 
-struct morph_semantic_a {
-	/* 0x00 */ uint16_t unknown_0;
-	/* 0x02 */ uint16_t unknown_2;
+struct morph_entry_desc_6 {
+	/* 0x00 */ uint16_t num_verts; // 0x0A00 unless last
+	/* 0x02 */ uint32_t unknown_2; // 0x50 unless last, flags maybe? 1st bit is always set on last chunk.
 };
 
-struct morph_semantic_e {
-	/* 0x00 */ uint16_t unknown_0;
-	/* 0x02 */ uint16_t unknown_2;
-	/* 0x04 */ uint16_t unknown_4;
-};
+// vertex stream is likely a multiple of 9, but this does not add up to the total vertex count. It will always have more than needed.
+// characters\enemy\enm_amoeboid_small_ps4\enm_amoeboid_small_ps4.model (file with smallest morph section)
+// 6 affected subsets: 1, 4, 6, 9, 12, 15
+// vertex byte sizes: 94188, 45848, 25264, 13544, 4088, 2012
+// index byte sizes: 1208, 588, 324, 176, 56, 28
+// subset vertex count: 9660, 4702, 2591, 1389, 419, 206
+// somehow this all relates to each other.
+// vertex size / vertex count = always 9.75~
+// vertex size - vertex count * 9 = x / (index size / 2) = 11~ or 12~
 
-struct morph_semantic_f {
-	/* 0x00 */ uint32_t unknown_0;
-	/* 0x04 */ uint16_t unknown_4;
-	/* 0x06 */ uint16_t unknown_a;
-	/* 0x08 */ uint32_t unknown_6;
-};
+// note: "vertex" and "index" are unverified assumptions 
 
 struct morph_entry {
 	/* 0x00 */ uint32_t id;
-	/* 0x04 */ uint32_t name_offset;
+	/* 0x04 */ uint32_t name_offset; // from start of DAT1
 	/* 0x08 */ uint32_t vertex_offset;
-	/* 0x0C */ uint32_t index_offset;
-	/* 0x10 */ uint32_t unknown;
+	/* 0x0C */ uint32_t index_offset; // maybe vertex_id list?
+	/* 0x10 */ uint32_t unknown; // always 0x02 ?? ?? 00
 	/* 0x14 */ float bounds[4]; // two points?
-	/* 0x24 */ uint16_t semantic_count;
-	/* 0x26 */ uint16_t semantic_descriptor_size; // size of struct - 0x30
+	/* 0x24 */ uint16_t subset_count;
+	/* 0x26 */ uint16_t descriptor_size; // size of struct - 0x30
 	/* 0x28 */ uint32_t vertex_size;
 	/* 0x2C */ uint32_t index_size;
-	// <start semantic descriptors, size = semantic_descriptor_size>
-	/* 0x30 */ uint8_t semantic[semantic_count]; // assumption. align!
-	morph_semantic_a semantic_a[semantic_count];
-	uint32_t semantic_b[semantic_count];
-	uint16_t semantic_c[semantic_count];
-	uint16_t semantic_d[semantic_count]; // looks like offsets
-	morph_semantic_e semantic_e[semantic_count];
-	morph_semantic_f semantic_f[semantic_count];
+	// <start descriptors, size = descriptor_size>
+	/* 0x30 */ uint8_t subset_ids[subset_count];
+	// align(4)
+	uint32_t vertex_offsets[subset_count];
+	uint32_t index_offsets[subset_count];
+	uint16_t vertex_counts[subset_count];
+	// align(4)
+	uint16_t chunk_start[subset_count];
+	// align(4)
+	morph_entry_desc_6 chunks[]; // until descriptor_size is reached. indexed by chunk_start.
+	// align(4)
 };
