3c82d79a3e01aa6810a4d8e782c6c3c02a5c23fe
Ada <yretenai@gmail.com>
2025-02-10 13:21:13 +0000

    dsar: handle some edge cases, use managed tilestream

--- Rivet/Data/DataStreamArchive.cs
+++ Rivet/Data/DataStreamArchive.cs
@@ -45,7 +45,7 @@ public sealed class DataStreamArchive : IDisposable, IAsyncDisposable {
 	public DSARHeader Header { get; }
 	public DSARChunk[] Chunks { get; } = [];
 	private static Iron Iron { get; } = new();
-
+	private const int ChunkSize = 0x8000000;
 	public ValueTask DisposeAsync() => BaseStream.DisposeAsync();
 
 	public void Dispose() => BaseStream.Dispose();
@@ -57,80 +57,88 @@ public sealed class DataStreamArchive : IDisposable, IAsyncDisposable {
 		ArgumentOutOfRangeException.ThrowIfGreaterThan(assetEnd, Header.Size, nameof(assetOffset));
 
 		var buffer = new RivetMemory<byte>(assetSize);
-		if (!IsCompressed) {
-			BaseStream.Position = assetOffset;
-			BaseStream.ReadExactly(buffer.Memory.Span);
-			return buffer;
-		}
 
-		var chunkIndex = -1;
-		// find the first chunk that contains the start offset
-		for (var i = 0; i < Chunks.Length; ++i) {
-			var chunk = Chunks[i];
-			if (chunk.Offset <= assetOffset && chunk.Offset + chunk.Size >= assetOffset) {
-				chunkIndex = i;
-				break;
-			}
-		}
-
-		if (chunkIndex == -1) {
-			throw new UnreachableException("chunk index is unknown, goodbye");
-		}
-
-		var localOffset = 0;
-		while (localOffset < assetSize) {
-			if (chunkIndex >= Chunks.Length) {
-				throw new UnreachableException("chunk index is greater than asset end, goodbye");
+		try {
+			var tmp = buffer;
+			if (!IsCompressed) {
+				BaseStream.Position = assetOffset;
+				BaseStream.ReadExactly(buffer.Memory.Span);
+				buffer = null;
+				return tmp;
 			}
 
-			var chunk = Chunks[chunkIndex++];
-
-			if (chunk.Offset > assetEnd) {
-				throw new UnreachableException("chunk offset is greater than asset end, goodbye");
-			}
-
-			var shift = localOffset == 0 && chunk.Offset < assetOffset ? (int) (assetOffset - chunk.Offset) : 0;
-			if (chunk.CompressionType == DSARCompression.Padding) {
-				localOffset += chunk.Size - shift;
-				continue;
-			}
-
-			using var uncompressedBuffer = MemoryPool<byte>.Shared.Rent(chunk.Size);
-			using var compressedBuffer = MemoryPool<byte>.Shared.Rent(chunk.CompressedSize);
-			BaseStream.Position = chunk.CompressedOffset;
-			BaseStream.ReadExactly(compressedBuffer.Memory[..chunk.CompressedSize].Span);
-
-			switch (chunk.CompressionType) {
-				case DSARCompression.None: {
-					compressedBuffer.Memory[..chunk.CompressedSize].CopyTo(uncompressedBuffer.Memory[..chunk.Size]);
+			var chunkIndex = -1;
+			// find the first chunk that contains the start offset
+			for (var i = 0; i < Chunks.Length; ++i) {
+				var chunk = Chunks[i];
+				if (chunk.Offset <= assetOffset && chunk.Offset + chunk.Size >= assetOffset) {
+					chunkIndex = i;
 					break;
 				}
-				case DSARCompression.Unknown1: {
-					throw new NotSupportedException("compression type 1 has never been seen before");
+			}
+
+			if (chunkIndex == -1) {
+				throw new UnreachableException("chunk index is unknown, goodbye");
+			}
+
+			var localOffset = 0;
+			using var uncompressedBuffer = MemoryPool<byte>.Shared.Rent(ChunkSize);
+			using var compressedBuffer = MemoryPool<byte>.Shared.Rent(ChunkSize);
+			while (localOffset < assetSize) {
+				if (chunkIndex >= Chunks.Length) {
+					throw new UnreachableException("chunk index is greater than asset end, goodbye");
 				}
-				case DSARCompression.GDeflate: {
-					if (!GDeflate.Decompress(compressedBuffer.Memory[..chunk.CompressedSize], uncompressedBuffer.Memory[..chunk.Size], 1)) {
-						throw new InvalidOperationException("gdeflate failure");
+
+				var chunk = Chunks[chunkIndex++];
+
+				if (chunk.Offset > assetEnd) {
+					throw new UnreachableException("chunk offset is greater than asset end, goodbye");
+				}
+
+				var shift = localOffset == 0 && chunk.Offset < assetOffset ? (int) (assetOffset - chunk.Offset) : 0;
+				if (chunk.CompressionType == DSARCompression.Padding) {
+					localOffset += chunk.Size - shift;
+					continue;
+				}
+
+				BaseStream.Position = chunk.CompressedOffset;
+				BaseStream.ReadExactly(compressedBuffer.Memory[..chunk.CompressedSize].Span);
+
+				switch (chunk.CompressionType) {
+					case DSARCompression.None: {
+						compressedBuffer.Memory[..chunk.CompressedSize].CopyTo(uncompressedBuffer.Memory[..chunk.Size]);
+						break;
 					}
+					case DSARCompression.Unknown1: {
+						throw new NotSupportedException("compression type 1 has never been seen before");
+					}
+					case DSARCompression.GDeflate: {
+						if (!GDeflate.Decompress(compressedBuffer.Memory[..chunk.CompressedSize], uncompressedBuffer.Memory[..chunk.Size])) {
+							throw new InvalidOperationException("gdeflate failure");
+						}
 
-					break;
+						break;
+					}
+					case DSARCompression.LZ4: {
+						using var lz4 = Iron.Decompress(Codec.LZ4, compressedBuffer.Memory[..chunk.CompressedSize].Span, chunk.Size);
+						lz4.AsSpan().CopyTo(uncompressedBuffer.Memory.Span);
+						break;
+					}
 				}
-				case DSARCompression.LZ4: {
-					using var lz4 = Iron.Decompress(Codec.LZ4, compressedBuffer.Memory[..chunk.CompressedSize].Span, chunk.Size);
-					lz4.AsSpan().CopyTo(uncompressedBuffer.Memory.Span);
-					break;
+
+				var copy = uncompressedBuffer.Memory[..chunk.Size].Span[shift..];
+				if (chunk.Offset + chunk.Size > assetEnd) {
+					copy = copy[..(assetSize - localOffset)];
 				}
+
+				copy.CopyTo(buffer.Memory.Span[localOffset..]);
+				localOffset += copy.Length;
 			}
 
-			var copy = uncompressedBuffer.Memory[..chunk.Size].Span[shift..];
-			if (chunk.Offset + chunk.Size > assetEnd) {
-				copy = copy[..(assetSize - localOffset)];
-			}
-
-			copy.CopyTo(buffer.Memory.Span[localOffset..]);
-			localOffset += copy.Length;
+			buffer = null;
+			return tmp;
+		} finally {
+			buffer?.Dispose();
 		}
-
-		return buffer;
 	}
 }
