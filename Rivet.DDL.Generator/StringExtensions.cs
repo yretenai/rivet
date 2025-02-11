125c278276f095d674dd49266d26cc448de8cc34
Ada <yretenai@gmail.com>
2025-02-11 14:15:08 +0000

    chore: remove unused keywords array

--- Rivet.DDL.Generator/StringExtensions.cs
+++ Rivet.DDL.Generator/StringExtensions.cs
@@ -7,14 +7,6 @@ using System.Text.RegularExpressions;
 namespace Rivet.DDL.Generator;
 
 public static partial class StringExtensions {
-	private static string[] Keywords = [
-		"abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum",
-		"event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long",
-		"namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc",
-		"static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while",
-	];
-
-
 	private static readonly Regex StripLineRegex = StripLineRegexGen();
 
 	private static readonly Regex CollapseLineRegex = CollapseLineRegexGen();
